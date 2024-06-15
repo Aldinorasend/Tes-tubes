0f1b8ed7a412760f0757bf82d06ae816e5e62352
Aldinorasend <aldinorasend@gmail.com>
2024-06-15 23:27:01 +0700

    test push

---
+++ MenuManager/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using OrderLibrary;
+
+namespace MenuManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var menuManager = new MenuManager<MenuManager.MenuItem>();
+
+            menuManager.AddItem(1, "Nasi Goreng", 15000);
+            menuManager.AddItem(2, "Mie Goreng", 12000);
+            menuManager.DisplayMenu();
+
+
+            Console.Read();
+
+        }
+    }
+}
